c19c23121770e55b870bac9331a2aae97bbc89f0
Welljones <wjmhate@gmail.com>
2024-02-18 13:45:06 +0800

    Fixing bugs, functions wont work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cells = document.querySelectorAll('.cell');
 const statusText = document.querySelector('#statusText');
 const restartBtn = document.querySelector('#restartBtn');
-const winCondition = [
+const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -9,9 +9,9 @@ const winCondition = [
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
+    [2, 4, 6]
 ];
-let option = ["","","","","","","","",""];
+let options = ["","","","","","","","",""];
 let currentPlayer = "X";
 let running = false;
 
@@ -27,23 +27,51 @@ function initializeGame(){
 function cellClicked(){
     const cellIndex = this.getAttribute("cellIndex");
 
-    if(option[cellIndex] != "" || !running){
-        return
+    if(options[cellIndex] != "" || !running){
+        return;
     }
     updateCell(this, cellIndex);
     checkWinner();
 }
 
 function updateCell(cell, index){
-
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
 }
 
 function changePlayer(){
-
+    currentPlayer = (currentPlayer == "X") ? "0" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
 }
 
 function checkWinner(){
+    let roundWon = false;
 
+    For(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} Wins!`;
+        running = false;
+    }
+    else if(!options.include("")){
+        statusText.textContent = `Draw!`;  
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
 }
 
 function restartGame(){
